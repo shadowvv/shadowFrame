40d9dd0db0e17bc5777aeffe21560dfe2d595c3f
shadowvx <57322722@163.com>
2019-06-11 11:27:50 +0800

    netty pipe test

--- shadowFrame/src/com/test/net/netty/pipeTest/SecondChannelHandler.java
+++ shadowFrame/src/com/test/net/netty/pipeTest/SecondChannelHandler.java
@@ -1,9 +1,13 @@
 package com.test.net.netty.pipeTest;
 
-import io.netty.channel.ChannelHandler;
-import io.netty.channel.ChannelHandlerContext;
+import java.net.SocketAddress;
 
-public class SecondChannelHandler implements ChannelHandler {
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandler;
+import io.netty.channel.ChannelOutboundHandler;
+import io.netty.channel.ChannelPromise;
+
+public class SecondChannelHandler implements ChannelInboundHandler,ChannelOutboundHandler {
 
 	@Override
 	public void handlerAdded(ChannelHandlerContext ctx) throws Exception {
@@ -18,6 +22,103 @@ public class SecondChannelHandler implements ChannelHandler {
 	@Override
 	public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
 		System.out.println("second exceptionCaught");
+	}
+
+	@Override
+	public void bind(ChannelHandlerContext ctx, SocketAddress localAddress, ChannelPromise promise) throws Exception {
+		System.out.println("second bind");
+		ctx.bind(localAddress, promise);
+	}
+
+	@Override
+	public void connect(ChannelHandlerContext ctx, SocketAddress remoteAddress, SocketAddress localAddress,
+			ChannelPromise promise) throws Exception {
+		System.out.println("second connect");
+		ctx.connect(remoteAddress, localAddress, promise);
+	}
+
+	@Override
+	public void disconnect(ChannelHandlerContext ctx, ChannelPromise promise) throws Exception {
+		System.out.println("second disconnect");
+		ctx.disconnect(promise);
+	}
+
+	@Override
+	public void close(ChannelHandlerContext ctx, ChannelPromise promise) throws Exception {
+		System.out.println("second close");
+		ctx.close();
+	}
+
+	@Override
+	public void deregister(ChannelHandlerContext ctx, ChannelPromise promise) throws Exception {
+		System.out.println("second deregister");
+		ctx.deregister(promise);
+	}
+
+	@Override
+	public void read(ChannelHandlerContext ctx) throws Exception {
+		System.out.println("second read");
+		ctx.read();
+	}
+
+	@Override
+	public void write(ChannelHandlerContext ctx, Object msg, ChannelPromise promise) throws Exception {
+		System.out.println("second write");
+		ctx.write(msg, promise);
+	}
+
+	@Override
+	public void flush(ChannelHandlerContext ctx) throws Exception {
+		System.out.println("second flush");
+		ctx.flush();
+	}
+
+	@Override
+	public void channelRegistered(ChannelHandlerContext ctx) throws Exception {
+		System.out.println("second channelRegistered");
+		ctx.fireChannelRegistered();
+	}
+
+	@Override
+	public void channelUnregistered(ChannelHandlerContext ctx) throws Exception {
+		System.out.println("second channelUnregistered");
+		ctx.fireChannelUnregistered();
+	}
+
+	@Override
+	public void channelActive(ChannelHandlerContext ctx) throws Exception {
+		System.out.println("second channelActive");
+		ctx.fireChannelActive();
+	}
+
+	@Override
+	public void channelInactive(ChannelHandlerContext ctx) throws Exception {
+		System.out.println("second channelInactive");
+		ctx.fireChannelInactive();
+	}
+
+	@Override
+	public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+		System.out.println("second channelRead");
+		ctx.fireChannelRead(msg);
+	}
+
+	@Override
+	public void channelReadComplete(ChannelHandlerContext ctx) throws Exception {
+		System.out.println("second channelReadComplete");
+		ctx.fireChannelReadComplete();
+	}
+
+	@Override
+	public void userEventTriggered(ChannelHandlerContext ctx, Object evt) throws Exception {
+		System.out.println("second userEventTriggered");
+		ctx.fireUserEventTriggered(evt);
+	}
+
+	@Override
+	public void channelWritabilityChanged(ChannelHandlerContext ctx) throws Exception {
+		System.out.println("second channelWritabilityChanged");
+		ctx.fireChannelWritabilityChanged();
 	}
 
 }
